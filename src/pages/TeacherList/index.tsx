0617e38c0b96d5a7396bc45f5872ec68c8e58723
Ivan L. Seibel <ivan@pop-os.localdomain>
2020-08-07 16:06:10 -0300

    Picker for weekday and toggle filters.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text } from 'react-native';
-import { ScrollView, TextInput } from 'react-native-gesture-handler';
-import Picker from 'react-native-picker-select';
+import {
+  ScrollView,
+  TextInput,
+  BorderlessButton,
+  RectButton,
+} from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Picker from '../../components/Picker';
 
 const TeacherList: React.FC = () => {
   const [selectedWeekday, setSelectedWeekday] = useState();
+  const [isFiltersvisible, setIsFiltersVisible] = useState(false);
   const weekdays = [
     { label: 'Sunday', value: 0 },
     { label: 'Monday', value: 1 },
@@ -20,38 +27,55 @@ const TeacherList: React.FC = () => {
     { label: 'Saturday', value: 6 },
   ];
 
+  const handleToggleShowFilters = useCallback(() => {
+    setIsFiltersVisible(state => !state);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Available Proffys">
-        <View style={styles.searchForm}>
-          <Text style={styles.label}>Subject</Text>
-          <TextInput
-            placeholderTextColor="#c1bccc"
-            style={styles.input}
-            placeholder="Which subject?"
-          />
+      <PageHeader
+        title="Available Proffys"
+        headerRight={
+          <BorderlessButton onPress={handleToggleShowFilters}>
+            <Feather name="filter" size={20} color="#fff" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersvisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Subject</Text>
+            <TextInput
+              placeholderTextColor="#c1bccc"
+              style={styles.input}
+              placeholder="Which subject?"
+            />
 
-          <View style={styles.inputGroup}>
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Weekday</Text>
-              <Picker items={weekdays} />
-              {/* <TextInput
-                placeholderTextColor="#c1bccc"
-                style={styles.input}
-                placeholder="Which weekday?"
-              /> */}
-            </View>
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Weekday</Text>
+                <Picker
+                  title="Choose a weekday"
+                  pickerValues={weekdays}
+                  placeholder="Which weekday?"
+                  inputStyle={styles.picker}
+                  onChange={setSelectedWeekday}
+                />
+              </View>
 
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Time</Text>
-              <TextInput
-                placeholderTextColor="#c1bccc"
-                style={styles.input}
-                placeholder="Which time?"
-              />
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Time</Text>
+                <TextInput
+                  placeholderTextColor="#c1bccc"
+                  style={styles.input}
+                  placeholder="Which time?"
+                />
+              </View>
             </View>
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Search</Text>
+            </RectButton>
           </View>
-        </View>
+        )}
       </PageHeader>
       <ScrollView
         showsVerticalScrollIndicator={false}
